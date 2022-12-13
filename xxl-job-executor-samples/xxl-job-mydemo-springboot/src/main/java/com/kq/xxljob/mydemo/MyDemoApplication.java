a1fa2ab60783453ab2abc26e65538c3c066b58f7
kong <king@qq.com>
2022-12-13 13:39:17 +0800

    添加mydemo例子

---
+++ xxl-job-executor-samples/xxl-job-mydemo-springboot/src/main/java/com/kq/xxljob/mydemo/MyDemoApplication.java
@@ -0,0 +1,16 @@
+package com.kq.xxljob.mydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuxueli 2018-10-28 00:38:13
+ */
+@SpringBootApplication
+public class MyDemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(MyDemoApplication.class, args);
+	}
+
+}
---
+++ xxl-job-executor-samples/xxl-job-mydemo-springboot/src/main/java/com/kq/xxljob/mydemo/job/MyDemoJob.java
@@ -0,0 +1,52 @@
+package com.kq.xxljob.mydemo.job;
+
+/**
+ * @author kq
+ * @date 2022-12-13 10:32
+ * @since 2020-0630
+ */
+
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * XxlJob开发示例（Bean模式）
+ *
+ * 开发步骤：
+ *      1、任务开发：在Spring Bean实例中，开发Job方法；
+ *      2、注解配置：为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ *      3、执行日志：需要通过 "XxlJobHelper.log" 打印执行日志；
+ *      4、任务结果：默认任务结果为 "成功" 状态，不需要主动设置；如有诉求，比如设置任务结果为失败，可以通过 "XxlJobHelper.handleFail/handleSuccess" 自主设置任务结果；
+ *
+ * @author xuxueli 2019-12-11 21:52:51
+ */
+@Component
+public class MyDemoJob {
+
+    private static Logger logger = LoggerFactory.getLogger(MyDemoJob.class);
+
+
+    /**
+     * 1、简单任务示例（Bean模式）
+     */
+    @XxlJob("demoJobHandler")
+    public void demoJobHandler() throws Exception {
+        XxlJobHelper.log("MyDemoJob XXL-JOB, Hello World.");
+
+        for (int i = 0; i < 2; i++) {
+            XxlJobHelper.log("beat at:" + i);
+            System.out.println("System.out beat at:" + i);
+            logger.info("logger beat at:" + i);
+            TimeUnit.SECONDS.sleep(2);
+        }
+        // default success
+    }
+
+
+
+}
